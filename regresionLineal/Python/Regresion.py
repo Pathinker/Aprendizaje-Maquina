bf0f9c6405f48e96550e4f57c51eb38cfabb28bf
DarchoG <danielguzmanzavala004@gmail.com>
2024-09-14 15:28:55 -0600

    Resolución manejo CSV usando pandas, otros casos y MSE

--- regresionLineal/Python/Regresion.py
+++ regresionLineal/Python/Regresion.py
@@ -1,5 +1,11 @@
-import numpy as np
+import io
+import sys
+import pandas as pd
 import matplotlib.pyplot as plt
+
+# Permitir la representación de acentos, porque uso otro metodo de encoding en el archivo CSV
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 def regresionLineal(Datos):
 
@@ -10,10 +16,10 @@ def regresionLineal(Datos):
 
     for i in range(len(Datos)):
 
-        sumatoriaX += Datos[i][0]
-        sumatoriaY += Datos[i][1]
-        sumatoriaProductos += Datos[i][0] * Datos[i][1]
-        sumatoriaXCuadrada += Datos[i][0] ** 2
+        sumatoriaX += Datos.iloc[i, 0]
+        sumatoriaY += Datos.iloc[i, 1]
+        sumatoriaProductos += Datos.iloc[i, 0] * Datos.iloc[i, 1]
+        sumatoriaXCuadrada += Datos.iloc[i, 0] ** 2
 
     m = ((len(Datos) * sumatoriaProductos) - (sumatoriaX * sumatoriaY)) / ((len(Datos) * sumatoriaXCuadrada) - (sumatoriaX ** 2))
     b = (sumatoriaY -  m * sumatoriaX) / len(Datos)
@@ -26,46 +32,93 @@ def regresionLineal(Datos):
 
     for i in range(len(Datos)):
 
-        diferenciaXY += (Datos[i][0] - promedioX) * (Datos[i][1] - promedioY)
+        diferenciaXY += (Datos.iloc[i, 0] - promedioX) * (Datos.iloc[i, 1] - promedioY)
 
-        diferenciaXCuadrada += (Datos[i][0] - promedioX) ** 2
-        diferenciaYCuadrada += (Datos[i][1] - promedioY) ** 2
+        diferenciaXCuadrada += (Datos.iloc[i, 0] - promedioX) ** 2
+        diferenciaYCuadrada += (Datos.iloc[i, 1] - promedioY) ** 2
 
     coeficienteCorrelacion = diferenciaXY / ((diferenciaXCuadrada ** (1/2)) * (diferenciaYCuadrada ** (1/2)))
 
     return m, b, coeficienteCorrelacion
 
-def graficarInformacion(Datos):
+def graficarInformacion(datosEntrenamiento, datosPrediccion = None):
 
-    m, b, coeficienteCorrelacion = regresionLineal(Datos)
+    m, b, coeficienteCorrelacion = regresionLineal(datosEntrenamiento)
 
     x = []
     y = []
+    prediccion = []
+    erroresPrediccion = 0
 
-    for i in range(len(Datos)):
+    for i in range(len(datosEntrenamiento)):
 
-        x.append(Datos[i][0])
-        y.append(Datos[i][1])
+        prediccion.append(datosEntrenamiento.iloc[i, 0] * m + b)
 
-    plt.scatter(x, y, color = "Blue")
-    #plt.scatter(x, prediccion, color = "Red")
+        x.append(datosEntrenamiento.iloc[i, 0])
+        y.append(datosEntrenamiento.iloc[i, 1])
+
+        erroresPrediccion += (x[i] - prediccion[i]) ** 2
+
+    erroresPrediccion /= len(datosEntrenamiento)
+
+    print("\nPendiente: {} \nCoordenada Origen (Y): {} \nCoeficiente de Correlación: {} \nError Cuádratico Medio (MSE): {}".format(m, b, coeficienteCorrelacion, erroresPrediccion))
+
+    plt.title("Entrenamiento del Modelo")
+    plt.scatter(x, y, color = "Blue", label = "Datos")
+    plt.xlabel("Salario Anual")
+    plt.plot(x, prediccion, color = "Red", label = "Regresión Lineal")
+    plt.ylabel("Costo Vehiculo")
+    plt.legend()
 
     plt.show()
 
+    if datosPrediccion is not None:
+                    
+        x = []
+        y = []
+        prediccion = []
+        erroresPrediccion = 0
+
+        for i in range(len(datosPrediccion)):
+
+            prediccion.append(datosPrediccion.iloc[i, 0] * m + b)
+
+            x.append(datosPrediccion.iloc[i, 0])
+            y.append(datosPrediccion.iloc[i, 1])
+            erroresPrediccion += (x[i] - prediccion[i]) ** 2
+
+        erroresPrediccion /= len(datosPrediccion)
+
+        print("Error Cuádratico Medio (MSE) con Datos no Conocidos: ", erroresPrediccion)
+    
+        plt.title("Desempeño del Modelo con Datos No Conocidos")
+        plt.scatter(x, y, color = "Blue", label = "Datos")
+        plt.xlabel("Salario Anual")
+        plt.scatter(x, prediccion, color = "Red", label = "Regresión Lineal")
+        plt.ylabel("Costo Vehiculo")
+        plt.legend()
+
+        plt.show()
+
 # Leer el CSV, solamente adquirir las columnas de información 5 y 8 que son las que contienen la información que necesito.
 
-datos = np.loadtxt(r"regresionLineal/Python/car_purchasing.csv", 
-                   skiprows = 1,  
-                   delimiter = ",", 
-                   usecols = [5, 8],
-                   dtype = float)
+datos = pd.read_csv(r"regresionLineal/Python/car_purchasing.csv", 
+                    delimiter=",", 
+                    usecols=[5, 8], 
+                    encoding='ISO-8859-1')
 
+print("Caso A: 100% Datos Entrenamiento y Predicción")
 graficarInformacion(datos)
 
-numeroEntranamiento =  int(len(datos) * 0.6)
+# Mezclara aleatoriamente los datos
 
-np.random.shuffle(datos)
+datos = datos.sample(frac=1).reset_index(drop=True) 
+
+# Separar en 60% Entrenamiento 40% Predicción
+
+numeroEntranamiento =  int(len(datos) * 0.6)
 arrayEntrenamiento = datos[:numeroEntranamiento]
 arrayPrediccion = datos[numeroEntranamiento:]
 
-print(regresionLineal(arrayEntrenamiento))
+print("\nCaso B: 60% Datos Entrenamiento y 40% Predicción")
+graficarInformacion(arrayEntrenamiento, arrayPrediccion)
