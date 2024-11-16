a54ff9c985ebb2c587d695842647f6977ee6b1ff
Pathinker <danielguzmanzavala004@gmail.com>
2024-11-16 01:57:31 -0600

    Integración Funciones Logaritmicas y Exponenciales para regualizar la exploración

--- SOM/SOM.py
+++ SOM/SOM.py
@@ -3,15 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-imagen = cv2.imread('SOM/Perro.jpg')
+X, Y = 50, 50
 imagenRGB = cv2.cvtColor(cv2.imread('SOM/Perro.jpg'), cv2.COLOR_BGR2RGB)
-#data = cv2.resize(imagenRGB, (227, 227)) / 255.0
+data = imagenRGB / 255.0 
 
-data = imagenRGB / 255.0
-
-somX, somY = 100, 100
+somX, somY = X, Y
 inputDimension = 3
-weights = np.random.rand(somX, somY, inputDimension)
+weights = np.zeros((somX, somY, inputDimension))
 
 def findBMU(inputVector, weights):
 
@@ -22,12 +20,15 @@ def findBMU(inputVector, weights):
 
 def updateWeights(inputVector, weights, bmuIndex, t, maxIter, initLearningRate = 0.5, initRadius = None):
 
+    alpha = 0.001
+    k = 0.001
+
     if(initRadius is None):
 
         initRadius = max(somX, somY) / 2
 
-    learningRate = initLearningRate * (1 - t / maxIter)
-    radius = initRadius * (1 - t / maxIter)
+    learningRate = initLearningRate * (1 / (1 + alpha * np.log(t + 1)))
+    radius = initRadius *  np.exp(-k * t)
 
     for i in range(somX):
 
@@ -41,26 +42,29 @@ def updateWeights(inputVector, weights, bmuIndex, t, maxIter, initLearningRate =
 
                 weights[i, j] += learningRate * influence * (inputVector - weights[i, j])
 
-iteraciones = 10000
+iteraciones = 2500
 
 for i in tqdm(range(iteraciones), desc="Entrenamiento SOM", unit = "Iteracion", dynamic_ncols=True):
 
     inputVector = data[np.random.randint(0, data.shape[0]), np.random.randint(0, data.shape[1]), :]
     
     bmuIndex = findBMU(inputVector, weights)
-    updateWeights(inputVector, weights, bmuIndex, i, iteraciones, initLearningRate = 0.1)
+    updateWeights(inputVector, weights, bmuIndex, i, iteraciones, initLearningRate = 1)
 
 fig, ax = plt.subplots(figsize=(8, 8))
 
 for i in range(somX):
-
     for j in range(somY):
-
         weight = weights[i, j]
-        ax.add_patch(plt.Rectangle((i, j), 1, 1, facecolor=weight))
+        ax.add_patch(plt.Rectangle((j, somY - i - 1), 1, 1, facecolor=weight))
 
 ax.set_xlim([0, somX])
 ax.set_ylim([0, somY])
 ax.set_title("Mapa SOM de Colores RGB")
+plt.axis('off')
+plt.show()
+
+plt.imshow(data)
+plt.title("Imagen Original")
 plt.axis('off')
 plt.show()
